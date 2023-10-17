b228da99ceba2b2ac7255be1908574ec10a82cbe
Bruno (Fzn) <119943937+bruno-rodrigues0@users.noreply.github.com>
2023-10-17 12:26:52 -0300

    Add files via upload

---
+++ aula01/index.php
@@ -0,0 +1,20 @@
+<?php
+echo'<h1>Teste de Impressao PHP</h1> ';
+?>
+
+<?= "Mais uma linha agora "?>
+
+<?php
+
+// comentario de uma linha
+
+/* comentario
+de
+varias
+linhas 
+*/
+
+
+echo '<hr>teste'
+
+?>

47ee09be7590e1197bba61897b28f9996174de6b
Bruno (Fzn) <119943937+bruno-rodrigues0@users.noreply.github.com>
2023-11-08 09:46:54 -0300

    Add files via upload

---
+++ aula02/atv01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="atv01.css">
+    <style>
+        h1{
+            background-color: aliceblue;
+        }
+    </style>
+</head>
+<body>
+
+    <h1 style="background-color: blue">h1 dentro do body</h1>
+
+</body>
+</html>
---
+++ aula02/exp01.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+    <style>
+
+h1 {
+    background-color: aquamarine;
+}
+
+p{
+    background-color: bisque;
+}
+
+h2{
+    background-color: blueviolet;
+}
+
+#p1{
+    background-color: brown;
+    color: aliceblue;
+    
+}</style>
+
+</head>
+<body>
+
+    <div class="para1">
+
+        <div class= "p1_h1">
+            <h1>Meu titulo</h1> 
+        </div> 
+
+        <br>
+        
+        <div class="p1_p1">
+            <p id="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis reiciendis explicabo ad distinctio eos est repudiandae, amet ullam corrupti eligendi, praesentium harum optio quae dignissimos et non, sed numquam adipisci!</p>
+        </div>
+
+    </div>
+
+
+    <div class="para2">
+
+        <div>
+            <h2>Meu titulo</h2>
+        </div>
+
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod doloribus corrupti quis a quisquam, iusto reprehenderit ea vero aliquam, facilis harum! Temporibus, aliquam! Ipsam necessitatibus aliquam non fugit sit nemo.</p>
+        </div>
+        
+    </div>
+
+</body>
+</html>

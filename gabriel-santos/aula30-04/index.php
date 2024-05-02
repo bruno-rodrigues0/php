dee2ba128724a1d9ccdc59cf9fc2640fffeaee0b
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-05-02 11:29:15 -0300

    Aula 30-04 feita

--- gabriel-santos/aula30-04/index.php
+++ gabriel-santos/aula30-04/index.php
@@ -17,18 +17,45 @@ $var2[1] = "salve";
 print_r($var2);
 
 foreach ($var2 as $teste1) {
-    $rgb1 = random_int(0, 255);
-    $rgb2 = random_int(0, 255);
-    $rgb3 = random_int(0, 255);
-    echo "<p style=\"background-color:rgb($rgb1,$rgb2,$rgb3);\">$teste1</p>";
+    $r = random_int(0, 255);
+    $g = random_int(0, 255);
+    $b = random_int(0, 255);
+    echo "<p style=\"background-color:rgb($r,$g,$b);\">$teste1</p>";
 };
 
+$aluno1["nome"] = "Gabriel";
+$aluno1["idade"] = 17;
+$aluno1["endereço"] = "Um Lugar";
+$aluno1["cidade"] = "Guanambi";
+$aluno1["estado"] = "Bahia";
+$aluno1["cpf"] = "001";
 
-$dados = array(
-    "Fábio" => 30, "Um Lugar", "Gbi", "BA", 001,
-    "Gabriel" => 17, "Um Lugar", "Gbi", "BA", 002
-);
+$aluno2["nome"] = "Bruno";
+$aluno2["idade"] = 17;
+$aluno2["endereço"] = "Um Lugar";
+$aluno2["cidade"] = "Montalvânia";
+$aluno2["estado"] = "Minas Gerais";
+$aluno2["cpf"] = "002";
 
-echo "<pre>";
-var_dump($dados);
-echo "</pre>";
+$professor["nome"] = "Fábio";
+$professor["idade"] = 30;
+$professor["endereço"] = "Um Lugar";
+$professor["cidade"] = "Guanambi";
+$professor["estado"] = "Bahia";
+$professor["cpf"] = "003";
+
+echo "Aluno 1: <br>";
+foreach ($aluno1 as $x) {
+    echo  key($aluno1) . ": $x  <br>";
+    next($aluno1);
+}
+echo "<br>Aluno 2: <br>";
+foreach ($aluno2 as $x) {
+    echo  key($aluno2) . ": $x  <br>";
+    next($aluno2);
+}
+echo "<br>Professor: <br>";
+foreach ($professor as $x) {
+    echo  key($professor) . ": $x  <br>";
+    next($professor);
+}

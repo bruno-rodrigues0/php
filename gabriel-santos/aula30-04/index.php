7e1b79eb59957465ec95ac20fa8f08641a61733e
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-02 12:04:30 -0300

    class aluno

--- gabriel-santos/aula30-04/index.php
+++ gabriel-santos/aula30-04/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('./class/aluno.php');
+
 $var = array("opa", 2, 3, 4);
 
 foreach ($var as $teste) {
@@ -23,39 +25,9 @@ foreach ($var2 as $teste1) {
     echo "<p style=\"background-color:rgb($r,$g,$b);\">$teste1</p>";
 };
 
-$aluno1["nome"] = "Gabriel";
-$aluno1["idade"] = 17;
-$aluno1["endereço"] = "Um Lugar";
-$aluno1["cidade"] = "Guanambi";
-$aluno1["estado"] = "Bahia";
-$aluno1["cpf"] = "001";
+$aluno1 = new Aluno('Gabriel', 17, "Rua Oscar Alho, 234", "Guanambi", "BA", "000.000.000-01");
+$aluno2 = new Aluno('Bruno', 17, "Rua Oscar Alho, 600", "Montalvânia", "MG", "000.000.000-02");
 
-$aluno2["nome"] = "Bruno";
-$aluno2["idade"] = 17;
-$aluno2["endereço"] = "Um Lugar";
-$aluno2["cidade"] = "Montalvânia";
-$aluno2["estado"] = "Minas Gerais";
-$aluno2["cpf"] = "002";
-
-$professor["nome"] = "Fábio";
-$professor["idade"] = 30;
-$professor["endereço"] = "Um Lugar";
-$professor["cidade"] = "Guanambi";
-$professor["estado"] = "Bahia";
-$professor["cpf"] = "003";
-
-echo "Aluno 1: <br>";
-foreach ($aluno1 as $x) {
-    echo  key($aluno1) . ": $x  <br>";
-    next($aluno1);
-}
-echo "<br>Aluno 2: <br>";
-foreach ($aluno2 as $x) {
-    echo  key($aluno2) . ": $x  <br>";
-    next($aluno2);
-}
-echo "<br>Professor: <br>";
-foreach ($professor as $x) {
-    echo  key($professor) . ": $x  <br>";
-    next($professor);
-}
+$aluno1 -> getInfo();
+echo "<br>";
+$aluno2 -> getInfo();
